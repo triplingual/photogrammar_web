8a1c0cf0ae97c7e59ef5238e3c99b5356abfd984
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2015-07-13 15:36:02 -0400

    Conversion to MySQLi
    Some related anti SQL injection improvements

--- records/index.php
+++ records/index.php
@@ -1,25 +1,34 @@
 <?php
 $page = search;
+$ERROR_MSG = "An error occurred. Please try again or send a message to <a href='mailto:thephotogrammar@gmail.com'>thephotogrammar@gmail.com</a> describing what happened.";
 $bodyopts = 'onload="initialize()"';
 include '../header.php';
 require_once 'login.php';
 
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
-if(!$db_server) die("Unable to connect to MySQL: " .mysql_error());
 
-mysql_select_db($db_database) or die('Unable to connect to MySQL: ' . mysql_error());
 if(isset($_GET['record']))
 {
-$fval['record'] = $_GET['record'];
-$thispic = $_GET['record'];
-
-    $query = "SELECT * FROM photo2 WHERE cnumber='" . $thispic . "'";
-    $result = mysql_query($query);
-    $ptitle = mysql_result($result, 0, 'title');
-    $pnom = mysql_result($result, 0, 'pname');
-    $pcall = mysql_result($result, 0, 'cnumber2');
     
+$mysqli = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+if ($mysqli->connect_error) {
+	if ($DEBUGGING)	{
+	    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+	}
+	else {
+	    die($ERROR_MSG);
+	}
 }
+if(!$mysqli) {
+	if ($DEBUGGING)	{
+		die("Unable to connect to MySQL: " .mysql_error());
+	}
+	else {
+		die($ERROR_MSG);
+	}
+}
+$fval['record'] = "";
+
 echo <<<_END
 
     <style>
@@ -105,41 +114,12 @@ _END;
 
 if(isset($_GET['record']))
 {
-    $fval['record'] = $_GET['record'];
-
-    $query = "SELECT * FROM photo2 WHERE cnumber='" . get_post('record'). "'";
-    //echo $query;
-    //echo '<br />';
-    //$query = "SELECT * FROM photo";
-    $result = mysql_query($query);
-    $ptitle = mysql_result($result, 0, 'title');
-    $pnom = mysql_result($result, 0, 'pname');
-    $pmon =  intval(mysql_result($result, 0, 'month'));
-    $pdate = $mons[$pmon] . mysql_result($result, 0, 'year');
-    $pstate = mysql_result($result, 0, 'state');
-    $pcounty = mysql_result($result, 0, 'county');
-    $pcity = mysql_result($result, 0, 'city');
-    $ploc = '<a class="record-text" href="/search/results.php?start=0&state=' .  mysql_result($result, 0, 'state') . '&year_start=1935&month_start=0&year_stop=1945&month_stop=12"  class="record-text">' . mysql_result($result, 0, 'state') . '</a>';
-    
-    if(mysql_result($result, 0, 'county') != "") $ploc = '<a class="record-text" href="/search/results.php?start=0&county=' .  mysql_result($result, 0, 'county') . '&state=' .  mysql_result($result, 0, 'state') . '&year_start=1935&month_start=0&year_stop=1945&month_stop=12"  class="record-text">' . mysql_result($result, 0, 'county') . '</a>' . ", " . $ploc;
-    if(mysql_result($result, 0, 'city') != "") $ploc = '<a class="record-text" href="/search/results.php?start=0&city=' .  mysql_result($result, 0, 'city') . '&county=' .  mysql_result($result, 0, 'county') . '&state=' .  mysql_result($result, 0, 'state') . '&year_start=1935&month_start=0&year_stop=1945&month_stop=12"  class="record-text">' . mysql_result($result, 0, 'city') . '</a>' . ", " . $ploc;
     
     
-    $pvannum0 = mysql_result($result, 0, 'van0');
-    $pvannum1 = mysql_result($result, 0, 'van1');
-    $pvannum2 = mysql_result($result, 0, 'van2');
-
-   
-    $plot = intval(mysql_result($result, 0, 'lotnum'));
-
+    
     if($ptitle == "") $ptitle = "None";
-
-/*     if($pdate == "0") $pdate = "Unknown"; */
     if($ploc == "") $ploc = "Unknown";
-/*     if($plot == 0) $plot = "None"; */
     if($pvannum0 == "" & $pvannum0 == "" & $pvannum0 == "") $pvannum0 = "None";
-
-    if (!$result) die ("Database access failed: " . mysql_error());
 
     echo '<div id="record-meta"><!--<h2>Record Information</h2>-->';
     echo '<div id="record-title"><h3 class="record-heading first">Caption <span style="font-size:.8em; color:grey; font-weight:normal;">(Original Description)</span></h3><span class="record-text">' . $ptitle . '</div><!--/#record-title--><div id="record-photographer"><h3 class="record-heading">Photographer</h3>';
@@ -147,7 +127,6 @@ if(isset($_GET['record']))
 
      if($pnom != "") {
    
-    echo '<a href="/search/results.php?start=0&pname=' .  $pnom . '&year_start=1935&month_start=0&year_stop=1945&month_stop=12" class="record-text">' .  $pnom . '</a>';
     }
 
      if($pnom == "") {
@@ -158,7 +137,6 @@ if(isset($_GET['record']))
     echo '</div><!--/#record-photographer-->';
     echo '<div id="record-created" class="record-field"><h3 class="record-heading">Created</h3>';
     if($pdate != "0") {
-  	  echo '<a class="record-text" href="/search/results.php?start=0&year_start=' . mysql_result($result, 0, 'year') . '&month_start=' . $pmon . '&year_stop=' . mysql_result($result, 0, 'year') . '&month_stop=' . $pmon . '">' . $pdate . '</a>';
     }
     if($pdate == "0") { echo '<span class="record-text">Unknown</span>';};
     echo '</div><!--/#record-created-->';
@@ -167,39 +145,25 @@ if(isset($_GET['record']))
      
     echo '<div id="record-notes" class="record-field"><h3 class="record-heading">Location</h3><span >' . $ploc . '</span></div><!--/#record-notes-->';
 	if ($pvannum1!="") {
-    echo '<div id="record-classification" class="record-field"><h3 class="record-heading">Classification<span style="font-size:.8em;color:grey; font-weight:normal;"> (Original Tagging System)</span></h3><span class="record-text"><ul><li><a class="record-text" style="font-size:1em;" href="/search/results.php?start=0&year_start=1935&month_start=0&year_stop=1945&month_stop=12&van=A' . $pvannum0 . '" >' .  $pvannum0 . '</a><ul><li><a class="record-text" style="font-size:1em;" href="/search/results.php?start=0&year_start=1935&month_start=0&year_stop=1945&month_stop=12&van=B' . $pvannum1 . '" >' .  $pvannum1 . '</a><ul><li><a class="record-text" style="font-size:1em;" href="/search/results.php?start=0&year_start=1935&month_start=0&year_stop=1945&month_stop=12&van=C' . $pvannum2 . '" >' .  $pvannum2 . '</a></li></ul></li></ul></li></ul></span></div><!--/#record-classification-->';
 	};
     echo '<div id="record-notes" class="record-field"><h3 class="record-heading">Lot Number<span style="font-size:.8em;color:grey; font-weight:normal;"> (Shooting Assignment)</span></h3>';
     if($plot != "0") {
-    	echo '<a class="record-text" href="/search/results.php?start=0&lot=' .  $plot . '&year_start=1935&month_start=0&year_stop=1945&month_stop=12">' . $plot . '</a>';
     };
     if($plot == "0") {
     echo '<span class="record-text">None</span>';
     }
     echo '</div><!--/#record-notes-->';
-//    echo '<div id="record-notes" class="record-field"><h3 class="record-heading">Notes</h3><span class="record-text"><p>' .  mysql_result($result, 0, 'pname') . '</p></span></div><!--/#record-notes-->';
-//    echo '<div id="record-subjects" class="record-field"><h3 class="record-heading">Subjects</h3><span class="record-text">' .  mysql_result($result, 0, 'pname') . '</span></div><!--/#record-subjects-->';
-//    echo '<div id="record-medium" class="record-field"><h3 class="record-heading">Medium</h3><span class="record-text">1 transparency : color.</span></div><!--/#record-medium-->';
-//    echo '<div id="record-call" class="record-field"><h3 class="record-heading">Call Number</h3><span class="record-text">LC-USW36-80</span></div><!--/#record-call-->';
-//    echo '<div id="record-reproduction" class="record-field"><h3 class="record-heading">Reproduction Number</h3><span class="record-text">LC-DIG-fsac-1a34889 DLC (digital file from original transparency)<br />LC-USW361-80 DLC (color film copy slide)</span></div><!--/#record-reproduction-->';
-//    echo '<div id="record-terms" class="record-field"><h3 class="record-heading">Special Terms of Use</h3><span class="record-text">No known restrictions on publication.</span></div><!--/#record-terms-->';
-//    echo '<div id="record-partof" class="record-field"><h3 class="record-heading">Part of</h3><span class="record-text">Farm Security Administration - Office of War Information Collection 12002-18</span></div><!--/#record-partof-->';
-//    echo '<div id="record-repository" class="record-field"><h3 class="record-heading">Repository</h3><span class="record-text">Library of Congress Prints and Photographs Division Washington, D.C. 20540 USA http://hdl.loc.gov/loc.pnp/pp.print</span></div><!--/#record-repository-->';
-    echo '<div id="record-digitalid" class="record-field"><h3 class="record-heading">Call Number<span style="font-size:.8em;color:grey; font-weight:normal;"> (Library of Congress)</span></h3><a class="record-text" target="_blank" href="http://www.loc.gov/pictures/item/' . $_GET['record'] . '">' . mysql_result($result, 0, 'cnumber2') . '</a></div><!--/#record-digitalid-->';
     
     echo '</div><!--/#record-meta-->';
     echo '<div id="record-image"><img src="';
-    $photourl = mysql_result($result, 0, 'large_url');
     if (substr($photourl, -2) != 'NA') {
   if ($photourl == '') {
 	      echo '/images/nophotolarge.png';
       }
       if ($photourl != '') {
-  	  	echo 'http://maps.library.yale.edu/images/public/photogrammar/' . mysql_result($result, 0, 'large_url');
   	  }
     }
     if (substr($photourl, -2) == 'NA') {
-  	  echo 'http://maps.library.yale.edu/images/public/photogrammar/' . mysql_result($result, 0, 'small_url');
     }
 
     
@@ -207,11 +171,9 @@ if(isset($_GET['record']))
 
 }
 
-mysql_close($db_server);
 
 function get_post($var)
 {
-    return mysql_real_escape_string($_GET[$var]);
 }
 
 echo <<<_END

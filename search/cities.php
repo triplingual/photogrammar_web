7c162853cc885da7bbac48b6aa96020670b76c5e
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2015-07-15 16:55:47 -0400

    Converted to use MySQLi

--- search/cities.php
+++ search/cities.php
@@ -1,26 +1,97 @@
 <?php
-header('Content-type: application/json');
+header('Content-type: application/json; charset=utf-8');
 
 require_once 'login.php';
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+require_once 'functions-search.php';
 
-if(!$db_server) die("Unable to connect to MySQL: " .mysql_error());
+$ERROR_MSG = "An error occurred. Please try again or send a message to <a href='mailto:thephotogrammar@gmail.com'>thephotogrammar@gmail.com</a> describing what happened.";
 
-mysql_select_db($db_database) or die('Unable to connect to MySQL: ' . mysql_error());
-
-$cityquery = "SELECT DISTINCT city FROM photo2 WHERE state =\"" . $_GET['state'] . "\"";
-if ($_GET['county'] !="") { $cityquery .= " AND county =\"" . $_GET['county'] . "\"";};
-
-$cityquery .= "AND city !=\"\" ORDER BY city ASC";
-/* echo $cityquery; */
-$cityresult = mysql_query($cityquery) or die(mysql_error());
-while($row = mysql_fetch_array($cityresult)){
-
-        $answer[] = array("id"=>$row['city'],"text"=>$row['city']);
-
-// finally encode the answer to json and send back the result.
+$mysqli = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+if ($mysqli->connect_error) {
+	if ($DEBUGGING)	{
+	    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+	}
+	else {
+	    die($ERROR_MSG);
+	}
 }
+if(!$mysqli) {
+	if ($DEBUGGING)	{
+		die("Unable to connect to MySQL: " .mysql_error());
+	}
+	else {
+		die($ERROR_MSG);
+	}
+}
+
+$state = get_post('state');
+$county = get_post('county');
+
+if ($county != "") {
+	// BUILD QUERYSTRING, ABSTRACTING VALUES AS APPROPRIATE
+	$cityquery = "SELECT DISTINCT city FROM photo WHERE state = ? AND county = ? AND city !=\"\" ORDER BY city ASC";
+	// PREPARE QUERY
+	if (!($stmt = $mysqli->prepare($cityquery))) {
+		if ($DEBUGGING)	{
+			die("Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error);
+		}
+		else {
+			die($ERROR_MSG);
+		}
+	}
+
+	// BIND VARIABLES
+	$stmt->bind_param( 'ss', $state, $county );
+
+} else { 
+	// BUILD QUERYSTRING, ABSTRACTING VALUES AS APPROPRIATE
+	$cityquery = "SELECT DISTINCT city FROM photo WHERE state = ? AND city !=\"\" ORDER BY city ASC";
+	// PREPARE QUERY
+	if (!($stmt = $mysqli->prepare($cityquery))) {
+		if ($DEBUGGING)	{
+			die("Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error);
+		}
+		else {
+			die($ERROR_MSG);
+		}
+	}
+
+	// BIND VARIABLES
+	$stmt->bind_param( 's', $state );
+}
+
+// Option for binding to multiple possible values, as seen at 
+// https://php.net/manual/en/mysqli-stmt.bind-param.php#104073
+/*
+$refArr = array($state, $county);
+$ref = new ReflectionClass('mysqli_stmt');
+$method = $ref->getMethod("bind_param");
+$method->invokeArgs($stmt, $refArr);
+*/
+// EXECUTE QUERY
+if (!$stmt->execute()) {
+	if ($DEBUGGING)	{
+		die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+	}
+	else {
+		die($ERROR_MSG);
+	}
+}
+// GET QUERY RESULTS
+/*
+	NOTE: Our webhost's PHP is not compiled with MySQL Native Driver (mysqlnd)
+	so we can't use mysqli_stmt::get_result(). To allow us to stick with 'SELECT *', 
+	though, we iterate through the returned fields in the metadata and bind that way.
+*/
+$stmt->store_result();
+$stmt->bind_result($city);
+
+while ($stmt->fetch()) {
+	$answer[] = array("id"=>$city, "text"=>$city);
+}
+// finally encode the answer to json and send back the result.
 echo '{ "results":';
 echo json_encode($answer);
+echo '}';
 ?>
-}
--- search/counties.php
+++ search/counties.php
@@ -1,22 +1,86 @@
 <?php
-header('Content-type: application/json');
+header('Content-type: application/json; charset=utf-8');
 
 require_once 'login.php';
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+require_once 'functions-search.php';
 
-if(!$db_server) die("Unable to connect to MySQL: " .mysql_error());
+$ERROR_MSG = "An error occurred. Please try again or send a message to <a href='mailto:thephotogrammar@gmail.com'>thephotogrammar@gmail.com</a> describing what happened.";
 
-mysql_select_db($db_database) or die('Unable to connect to MySQL: ' . mysql_error());
+$mysqli = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+if ($mysqli->connect_error) {
+	if ($DEBUGGING)	{
+	    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+	}
+	else {
+	    die($ERROR_MSG);
+	}
+}
+if(!$mysqli) {
+	if ($DEBUGGING)	{
+		die("Unable to connect to MySQL: " .mysql_error());
+	}
+	else {
+		die($ERROR_MSG);
+	}
+}
 
-$countyquery = "SELECT DISTINCT county FROM photo2 WHERE state =\"" . $_GET['state'] . "\" ORDER BY county ASC LIMIT 0,99";
-$countyresult = mysql_query($countyquery) or die(mysql_error());
-while($row = mysql_fetch_array($countyresult)){
+$state = get_post('state');
 
-        $answer[] = array("id"=>$row['county'],"text"=>$row['county']);
+// BUILD QUERYSTRING, ABSTRACTING VALUES AS APPROPRIATE
+$countyquery = "SELECT DISTINCT county FROM photo WHERE state = ? ORDER BY county ASC LIMIT 0,99";
+
+// PREPARE QUERY
+if (!($stmt = $mysqli->prepare($countyquery))) {
+	if ($DEBUGGING)	{
+		die("Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error);
+	}
+	else {
+		die($ERROR_MSG);
+	}
+}
+
+// BIND VARIABLES
+if (!$stmt->bind_param( 's', $state )) {
+	if ($DEBUGGING)	{
+		die("Binding parameters failed: (ERROR #" . $stmt->errno . ", ERROR MESSAGE: " . $stmt->error . " )<br />\n");
+	}
+	else {
+		die($ERROR_MSG);
+	}
+}
+// EXECUTE QUERY
+if (!$stmt->execute()) {
+	if ($DEBUGGING)	{
+		die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+	}
+	else {
+		die($ERROR_MSG);
+	}
+}
+// GET QUERY RESULTS
+/*
+	NOTE: Our webhost's PHP is not compiled with MySQL Native Driver (mysqlnd)
+	so we can't use mysqli_stmt::get_result(). To allow us to stick with 'SELECT *', 
+	though, we iterate through the returned fields in the metadata and bind that way.
+*/
+$stmt->store_result();
+$stmt->bind_result($county);
+
+$stmt->data_seek(0);
+
+while ($stmt->fetch()) {
+	$answer[] = array("id"=>$county, "text"=>$county);
+}
 
 // finally encode the answer to json and send back the result.
-}
 echo '{ "results":';
 echo json_encode($answer);
+echo '}';
+/*
+echo '{ "results":[{"id":"","text":""},{"id":"Fairfield","text":"Fairfield"},{"id":"Hartford","text":"Hartford"},{"id":"Litchfield","text":"Litchfield"},{"id":"Middlesex","text":"Middlesex"},{"id":"New Haven","text":"New Haven"},{"id":"New London","text":"New London"},{"id":"Tolland","text":"Tolland"},{"id":"Windham","text":"Windham"}]}';
+
+
+echo '{ "results":[{"id":"","text":""},{"id":"Fairfield","text":"Fairfield"},{"id":"Hartford","text":"Hartford"},{"id":"Litchfield","text":"Litchfield"},{"id":"Middlesex","text":"Middlesex"},{"id":"New Haven","text":"New Haven"},{"id":"New London","text":"New London"},{"id":"Tolland","text":"Tolland"},{"id":"Windham","text":"Windham"}]}';
+*/
 ?>
-}

a9743bfe3155a9cf2172b91904739a50fd88bd0b
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2015-07-16 10:32:27 -0400

    Initial commit to version control

---
+++ about/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url=http://photogrammar.yale.edu">
+        <script type="text/javascript">
+            window.location.href = "http://photogrammar.yale.edu"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow the <a href='http://photogrammar.yale.edu'>link</a>
+    </body>
+</html>

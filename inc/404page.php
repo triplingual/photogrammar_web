ac14cc928930792e7dda0869984c61d201c04f02
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2015-07-20 11:26:51 -0400

    Initial commit to version control

---
+++ inc/404page.php
@@ -0,0 +1,18 @@
+<?php
+$page = "404"; 
+include '../header.php';
+?>
+
+    <!-- Main jumbotron -->
+    <div class="jumbotron" style="padding-top:100px">
+      <div class="container">
+        <h1>404</h1>
+        <a href="/records/index.php?record=oem2002003167/PP"><img src="http://cdn.loc.gov/service/pnp/fsa/8b03000/8b03200/8b03220r.jpg" /></a>
+		<p style="margin-top:2em; margin-bottom:1em;">Nope, that page isn't in our collection.</p>
+        <p><a class="btn btn-primary btn-lg" href="/map" role="button">How about a map? &raquo;</a></p>
+      </div>
+    </div>
+
+<?php
+include '../footer.php';
+?>
